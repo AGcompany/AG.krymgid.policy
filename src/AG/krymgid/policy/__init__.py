76699fb3fe13362659a3e237946c1618eb0bedb6
Bogdan Girman <bogdan.girman@gmail.com>
2012-07-17 22:03:39 +0300

    Update

--- src/AG/krymgid/policy/__init__.py
+++ src/AG/krymgid/policy/__init__.py
@@ -1,1 +1,8 @@
 # This package may contain traces of nuts
+# -*- coding: utf-8 -*-
+
+from zope.i18nmessageid import MessageFactory as BaseMessageFactory
+MessageFactory = BaseMessageFactory('AG.krymgid.policy')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
